e4ee878fdae3a945aac1f59ece17e214a217da52
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-06-14 16:00:10 -0400

    humanizer block

--- src/components/DisplayItem.js
+++ src/components/DisplayItem.js
@@ -1,15 +1,17 @@
 import axios from 'axios'
 import React, {useState} from 'react'
+import RefineNumber from 'react-refine-number'
 
 const DisplayItem = (props) => {
     const [cartItem, setCartItem] = useState({...props.inventoryItem, quantity: 1})
+
 
     return (
         <>
             <img src={props.inventoryItem.image} alt={props.inventoryItem.title} />
             <h2>{props.inventoryItem.title}</h2>
             <p>{props.inventoryItem.description}</p>
-            <h3>${props.inventoryItem.price}</h3>
+            <h3>$<RefineNumber number= {props.inventoryItem.price}/>.00</h3>
             <button value={props.inventoryItem} onClick={(event) => props.handleAddToCart(cartItem)}>Add To Cart</button>
         </>
     )
