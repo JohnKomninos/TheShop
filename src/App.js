f88fdb1067bb0360c89c0b734fed767d3d3e2c54
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2022-06-14 14:31:09 -0500

    added basic header with ternary page change functionality

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 import DisplayItem from './components/DisplayItem'
 import Index from './components/Index'
+import Header from './components/Header'
 
 const App = () => {
 
   const [inventory, setInventory] = useState()
   const [cart, setCart] = useState([])
+  const [page, setPage] = useState('home')
 
   const getInventory = () => {
     axios.get('https://the-shop-back-end.herokuapp.com/api/inventory').then((response) => {
@@ -21,15 +23,30 @@ const App = () => {
     })
   }
 
+  const viewHome = () => {
+    setPage('home')
+  }
+
+  const viewShop = () => {
+    setPage('shop')
+  }
+
+  const viewCart = () => {
+    setPage('cart')
+  }
+
   useEffect(() => {
     getInventory()
   }, [])
 
   return (
     <>
-      <h1>Welcome to The Shop!</h1>
-      {inventory ? <Index inventory={inventory} /> : null}
-      <div className='inventory-container'>
+      <Header viewHome={viewHome} viewShop={viewShop} viewCart={viewCart} />
+      {page == 'home' ?
+        inventory ? <Index inventory={inventory} /> : null
+      : null}
+      {page == 'shop' ?
+        <div className='inventory-container'>
         {inventory?.map((inventoryItem) => {
           return (
             <div className='inventory-item' key={inventoryItem.id}>
@@ -37,7 +54,11 @@ const App = () => {
             </div>
           )
         })}
-      </div>
+        </div>
+      : null}
+      {page == 'cart' ?
+        <>PUT CART COMPONENT HERE</>
+      : null}
     </>
   )
 }
