698027803c8e91737ab09bc1b9db21bc80e7be74
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-06-14 16:18:20 -0400

    inline

--- src/components/DisplayItem.js
+++ src/components/DisplayItem.js
@@ -5,14 +5,17 @@ import RefineNumber from 'react-refine-number'
 const DisplayItem = (props) => {
     const [cartItem, setCartItem] = useState({...props.inventoryItem, quantity: 1})
 
+    let number = <RefineNumber number = {props.inventoryItem.price}/>
 
     return (
         <>
             <img src={props.inventoryItem.image} alt={props.inventoryItem.title} />
             <h2>{props.inventoryItem.title}</h2>
             <p>{props.inventoryItem.description}</p>
-            <h3>$<RefineNumber number= {props.inventoryItem.price}/>.00</h3>
-            <button value={props.inventoryItem} onClick={(event) => props.handleAddToCart(cartItem)}>Add To Cart</button>
+            <h3 className = 'inline'>$</h3>
+            <h3 className = 'inline'>{number}</h3>
+            <h3 className = 'inline'>.00</h3>
+            <button className = 'block' value={props.inventoryItem} onClick={(event) => props.handleAddToCart(cartItem)}>Add To Cart</button>
         </>
     )
 }
